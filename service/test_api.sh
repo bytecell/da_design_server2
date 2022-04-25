b5b96edfb77dfff32366d0d93aa91ced8d8af30c
Young-Seob Jeong <ysjay@chungbuk.ac.kr>
2022-04-26 08:14:53 +0900

    api test of predict

--- service/test_api.sh
+++ service/test_api.sh
@@ -4,4 +4,7 @@ else
         if [ $1 = 'list' ]; then
                 curl -H "Content-type: application/json" -X POST -d '{"top_k":"10"}' http://203.255.81.132:10101/api-list >> ret
         fi
+	if [ $1 = 'predict' ]; then
+                curl -H "Content-type: application/json" -X POST -d '{"company_name":"삼성전자"}' http://203.255.81.132:10101/api-predict >> ret
+        fi
 fi
