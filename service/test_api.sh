f0af3aeac3c2977677ba93fc5a190c8dad4d562a
Young-Seob Jeong <ysjay@chungbuk.ac.kr>
2026-04-25 17:19:59 +0900

    data_collect updated

--- service/test_api.sh
+++ service/test_api.sh
@@ -1,10 +1,10 @@
 if [ $# -lt 1 ]; then
         echo 'give 1 argument'
 else
-        if [ $1 = 'list' ]; then
+    if [ $1 = 'list' ]; then
                 curl -H "Content-type: application/json" -X POST -d '{"top_k":"10"}' http://203.255.81.132:10101/api-list >> ret
-        fi
+    fi
 	if [ $1 = 'predict' ]; then
                 curl -H "Content-type: application/json" -X POST -d '{"company_name":"삼성전자"}' http://203.255.81.132:10101/api-predict >> ret
-        fi
+    fi
 fi
--- src/data_collect.py
+++ src/data_collect.py
@@ -5,45 +5,50 @@ import datetime
 from da_design_server2.src import mylogger, myconfig
 import pdb
 
-def crawl_stock(logger, market='kospi', limit=60):
-    """Collect data from www.sedaily.com, and return data pairs.
+def crawl_stock(logger, limit=30):
+    """Collect data from a copy webpage of NPay kospi, and return data pairs.
 
     :param logger: logger instance
     :type logger: logging.Logger
     :param market: "kospi" or "kosdaq" (default "kospi")
     :type marget: str
-    :param limit: maximum # of items (default 60)
+    :param limit: maximum # of items (default 30), NEVER > 30 because this is just a copy webpage
     :type limit: int
     :return: pairs of {company: stock}
     :rtype: dict
     """
-    market = 1 if market == "kospi" else 2
-    root_url = \
-        ('https://www.sedaily.com/Stock/Content/StockInfoAjax?market=%d' % market) + \
-        '&Sorting=0&Page=%d&Period=1&SubOrder=Desc&SubSorting=0'
+    url = 'http://10.255.81.97/for_class/Npay.html'
 
     stocks = {}
 
     n_got = 0 # # of current items
     page = 1 # webpage index
-    while n_got < limit:
-        url = root_url % page
-        response = requests.get(url)
-        soup = BeautifulSoup(response.content, 'html.parser')
 
-        tr_list = [tr for tr in soup.select('tbody.no_line tr') if 'id' in tr.attrs]
-        if not tr_list:
+    if limit > 30 or limit < 0:
+        limit = 30
+        logger.info('Invalid #items lmit. So, it is forced to be 30, now.')
+    logger.info('#items limit = {}'.format(limit))
+
+    response = requests.get(url, timeout=10)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    
+    tr_list = soup.select('table.type_5 tbody tr')
+    for tr in tr_list:
+        name_tag = tr.select_one('a.tltle')
+        price_tags = tr.select('td.number')
+
+        if not name_tag or not price_tags or len(price_tags) <= 2:
+            continue
+
+        price = price_tags[1].get_text(strip=True).replace(',', '')
+        name = name_tag.get_text(strip=True)
+
+        stocks[name] = int(price)
+        n_got += 1
+        logger.info('collected: {}={}'.format(name, price))
+
+        if n_got >= limit:
             break
-
-        for tr in tr_list:
-            name = tr.select('td span a span')[1].text
-            value = int(tr.select('td span.td_position')[1].text.replace(',', ''))
-            stocks[name] = value
-            n_got += 1
-            if n_got == limit:
-                break
-
-        page += 1
 
     logger.info('{} items collected.'.format(n_got))
     return stocks
@@ -53,9 +58,7 @@ if __name__ == '__main__':
     cfg = myconfig.get_config('{}/share/project.config'.format(project_root_path))
     log_path = cfg['logger'].get('log_directory')
     logger = mylogger.get_logger(log_path)
-    
     ret = crawl_stock(logger)
-    pdb.set_trace()
 
 
 
